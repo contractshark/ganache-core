0b75b210a6817aba55981eb6c0451a6e7688b3d1
Mike Seese <seesemichaelj@gmail.com>
2021-03-16 20:34:00 -0700

    add comment for why dbDirectory is needed

--- src/chains/filecoin/filecoin/src/blockchain.ts
+++ src/chains/filecoin/filecoin/src/blockchain.ts
@@ -80,6 +80,7 @@ export default class Blockchain extends Emittery.Typed<
 
   readonly #database: Database;
 
+  // This is primarily used by Ganache UI to support workspaces
   get dbDirectory(): string | null {
     return this.#database.directory;
   }
